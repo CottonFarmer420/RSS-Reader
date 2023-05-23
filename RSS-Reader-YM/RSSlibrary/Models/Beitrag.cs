4e8609ce53c6226414dd7aba45783644db11a353
Maximilian Scharler <mscharler@schueler.hakzell.at>
2023-05-23 13:10:18 +0200

    2023-05-23 | 13:10

---
+++ RSS-Reader-YM/RSSlibrary/Models/Beitrag.cs
@@ -0,0 +1,17 @@
+﻿namespace RSSlibrary.Models
+{
+    public class Beitrag
+    {
+        public Guid BeitragID { get; set; }
+
+        public string link { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public DateTime pubDate { get; set; }
+
+
+    }
+}
---
+++ RSS-Reader-YM/RSSlibrary/Models/Feed.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSSlibrary.Models
+{
+    public class Feed
+    {
+        public string FeedID { get; set; } = string.Empty;
+
+        public string title { get; set; } = string.Empty;
+
+        public string link { get; set; } = string.Empty;
+
+        public string description { get; set; } = string.Empty;
+
+        public Beitrag b { get; set; }
+
+        public Feed(string title, string FeedID, string link) 
+        { 
+            this.FeedID = Guid.NewGuid().ToString();
+            this.title = title;
+            this.FeedID = FeedID;
+            this.link = link;
+            this.b = new Beitrag();
+        }
+
+
+
+    }
+}
