de73f50399a4284e79d937d7dd4052fd8becec8d
Maximilian Scharler <mscharler@schueler.hakzell.at>
2023-06-15 15:02:35 +0200

    update

--- RSS-Reader-YM/RSSlibrary/Services/XML-Repository.cs
+++ RSS-Reader-YM/RSSlibrary/Services/XML-Repository.cs
@@ -2,14 +2,43 @@
 using RSSlibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RSSlibrary.Services
 {
-    public class XML_Repository 
+    public class XML_Repository : IRepository
     {
-        
+        public List<Beitrag> Download(string url)
+        {
+            
+        }
+
+        public bool Favourite(Beitrag B)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNotRead(bool status)
+        {
+            return false;
+        }
+
+        public bool IsRead(bool status)
+        {
+            return true;
+        }
+
+        public bool SaveFeed(Feed feed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveReadBeitrag(Beitrag B, bool status)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
