d02ffa8f4836e308a0743ecc7611ce6a4c18d724
Maximilian Scharler <mscharler@schueler.hakzell.at>
2023-06-16 19:13:14 +0200

    update

--- RSS-Reader-YM/RSSlibrary/Models/Beitrag.cs
+++ RSS-Reader-YM/RSSlibrary/Models/Beitrag.cs
@@ -10,18 +10,15 @@
 
         public string PubDate { get; set; } = string.Empty;
 
-        public string Img { get; set; } = string.Empty;
-
         public bool Status { get; set; } = false;
 
-        public Beitrag(string url, string title, string description, string PubDate, bool status, string img)
+        public Beitrag(string url, string title, string description, string PubDate, bool status)
         {
             this.URL = url;
             this.Title = title;
             this.Description = description;
             this.PubDate = PubDate;
             this.Status = status;
-            this.Img = img;
         }
 
         public Beitrag()
